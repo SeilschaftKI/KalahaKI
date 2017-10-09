012ec5ddcec22657f13828128b6eab3e4a4d90db
Dummyrunner <32433506+Dummyrunner@users.noreply.github.com>
2017-10-09 15:18:34 +0200

    Grundlagen der Player-Klassen added

---
+++ Kalaha/HumanPlayer.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Benutzer: Daniel
+ * Datum: 09.10.2017
+ * Zeit: 12:54
+ */
+
+
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace Kalaha
+{
+	/// <summary>
+	/// Description of HumanPlayer.
+	/// </summary>
+	public class HumanPlayer : Player, IChooseMove
+	{
+		public int ChooseMove()
+		{
+			string input = Console.ReadLine();
+			
+			return Int32.Parse(input);
+			
+		}
+		
+		public HumanPlayer(int PlaySide)
+		{
+			this.PlaySide = PlaySide;
+		}
+		
+		public int getPlaySide()
+		{
+			return this.PlaySide;
+		}
+	}
+}
