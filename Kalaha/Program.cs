e2b26e5d9c17e9095510e919684a8cf55995e8c5
Daniel Bergmann <bergmann.da@gmx.de>
2020-03-16 12:57:32 +0100

    fixed missing .OfType duch namespace System.Linq

--- Kalaha/Program.cs
+++ Kalaha/Program.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Linq;
 using System.Windows.Forms;
 using System.Collections.Generic;
 
